dba9b8486aa798dccc2a5412e9af43e6b91bfd81
Jamie Mellway <jamie.mellway@gmail.ca>
2014-02-16 14:03:14 -0500

    Update

--- BicycleWorldObjectModel/ShoppingCartItem.cs
+++ BicycleWorldObjectModel/ShoppingCartItem.cs
@@ -4,21 +4,21 @@ using System.Runtime.Serialization;
 namespace BicycleWorldObjectModel
 {
     // Shopping cart item
-    [Serializable]
-    [DataContract]
-    public class ShoppingCartItem
-    {
-        [DataMember]
-        public int ProductID { get; set; }
-        [DataMember]
-        public string ProductNumber { get; set; }
-        [DataMember]
-        public string ProductName { get; set; }
-        [DataMember]
-        public decimal Cost { get; set; }
-        [DataMember]
-        public int Quantity { get; set; }
-        [DataMember]
-        public int OrderBy { get; set; }
+        [Serializable]
+        [DataContract]
+        public class ShoppingCartItem
+        {
+            [DataMember]
+            public int ProductID { get; set; }
+            [DataMember]
+            public string ProductNumber { get; set; }
+            [DataMember]
+            public string ProductName { get; set; }
+            [DataMember]
+            public decimal Cost { get; set; }
+            [DataMember]
+            public int Quantity { get; set; }
+            [DataMember]
+            public int OrderBy { get; set; }
+        }
     }
-}
